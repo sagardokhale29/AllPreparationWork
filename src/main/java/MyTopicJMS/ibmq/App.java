7bb203612cb1687369efcdbcbe6969a7f65994ce
Sagar Dokhale <sagar.dokhale@intellectdesign.com>
2019-09-27 14:42:53 +0530

    changes for ibmmq

---
+++ src/main/java/MyTopicJMS/ibmq/App.java
@@ -0,0 +1,13 @@
+package MyTopicJMS.ibmq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
